32bb0f861d29858f05766791cd2f7e9e496f1906
Gabriel <glimone.nunes@outlook.com>
2024-12-09 12:14:09 -0300

    Validate process number

--- scripts/data_process.py
+++ scripts/data_process.py
@@ -17,5 +17,12 @@ def build_dataframe():
 df = build_dataframe()
 
 
-def validate_number_process(value):
-    pattern = 
+def validate_numero_processo(value):
+    pattern = r'^\d{3}.\d{8}/\d{4}-\d{2}'
+    
+    return f"{bool(re.match(pattern, value)), {value}}"
+
+
+
+value = input("Number process: ")
+print(validate_numero_processo(value))
