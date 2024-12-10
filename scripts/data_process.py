defb0779b38e59828ac20024d568af2425182c7d
Gabriel <glimone.nunes@outlook.com>
2024-12-10 13:38:48 -0300

    Validate atribuição

--- scripts/data_process.py
+++ scripts/data_process.py
@@ -33,14 +33,39 @@ def validate_lote(value):
     match = re.fullmatch(pattern, value, re.IGNORECASE)
 
     if match:
-        return f'Número do lote: {match.group(2)}'
+        return match.group(2)
     else:
         return None
     
 
 v1 = input("Lote: ")
-print(validate_lote(v1))
+lote = validate_lote(v1)
+
+if lote:
+    lote = int(lote)
+else:
+    print("Lote inválido")
+    Lote = None
 
 
+def validate_atribuicao(lote):
+    atribuition_map = {
+        "Cirilo": [1, 16, 22, 28],
+        "Luiz": [13, 21, 24, 25, 30],
+        "Manuel": [6, 9, 11, 12, 19, 20, 31],
+        "Thaís": [3, 7, 23, 27, 29],
+    }
+
+    try:
+        lote_num = int(lote)
+    except ValueError:
+        return "Lote inválido"
+    
+    for tecnico, lotes in atribuition_map.items(): 
+        if lote_num in lotes:
+            return f"Técnico responsável: {tecnico}"
+        
+    return "Técnico indefinido"
 
 
+print(validate_atribuicao(lote))
